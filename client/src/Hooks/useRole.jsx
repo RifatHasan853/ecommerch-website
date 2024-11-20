da3506f02bd4afc82e7099402f2c487cc9911bcd
Rifat Hasan <155254027+RifatHasan853@users.noreply.github.com>
2024-11-20 21:57:30 +0600

    Update useRole

--- client/src/Hooks/useRole.jsx
+++ client/src/Hooks/useRole.jsx
@@ -6,7 +6,6 @@ import useAuth from './useAuth';
 const useRole = () => {
   const { user, loading } = useAuth();
 
-  // Use React Query to fetch the role based on the user's email
   const { data: role = '', isLoading } = useQuery({
     queryKey: ['role', user?.email],
     enabled: !loading && !!user?.email,  // Only fetch if user is authenticated and not loading
