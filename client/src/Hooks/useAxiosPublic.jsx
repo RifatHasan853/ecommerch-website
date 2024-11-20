1b1823e111b9725c304219c509727a34ad2d502f
Rifat Hasan <155254027+RifatHasan853@users.noreply.github.com>
2024-11-20 21:53:40 +0600

    Update useAxiosPublic

--- client/src/Hooks/useAxiosPublic.jsx
+++ client/src/Hooks/useAxiosPublic.jsx
@@ -1,4 +1,6 @@
 import axios from "axios";
+//import axios from "axios";
+
 const axiosPublic = axios.create({
     baseURL: 'https://ecommerch-server-vintnoryc-rifat-hasans-projects.vercel.app'
 })
